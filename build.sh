bec4433689096b85bb974a0f8764ec702932ac10
Sirathi Sawangnan <sawangnan@gmail.com>
2026-08-24 21:59:00 +0700

    Auto create superuser on deploy

--- build.sh
+++ build.sh
@@ -4,3 +4,12 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# สร้าง Superuser อัตโนมัติถ้ายังไม่มี
+python manage.py shell << EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin1234')
+    print('Superuser created successfully.')
+EOF
