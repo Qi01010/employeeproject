008e08db1d12a38cce976d7c92ee4fafa8c634c8
Sirathi Sawangnan <sawangnan@gmail.com>
2026-09-01 10:57:55 +0700

    Add CSRF_TRUSTED_ORIGINS for Render domain

--- employeeproject/settings.py
+++ employeeproject/settings.py
@@ -123,3 +123,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # เพิ่มต่อท้ายด้านล่างสุดของ settings.py
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+CSRF_TRUSTED_ORIGINS = [
+    'https://employeeproject-671320115.onrender.com',
+    'https://*.onrender.com',
+]
